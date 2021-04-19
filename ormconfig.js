d6a0af78ee25996ce1e385caaab858b769947bff
Shivraj Singh <nv-shivraj>
2021-04-19 19:00:28 +0530

    ORM file path updated

--- ormconfig.js
+++ ormconfig.js
@@ -3,21 +3,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const {
-  DB_TYPE,
-  DB_HOST,
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_DATABASE,
+    DB_TYPE,
+    DB_HOST,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_DATABASE,
 } = process.env;
 
 module.exports = {
-  type: DB_TYPE,
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-  migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
-  entities: [__dirname + '/src/**/*.entity.{ts,js}'],
+    type: DB_TYPE,
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
+    entities: [__dirname + '/src/modules/public/**/*.entity.{ts,js}'],
 };
