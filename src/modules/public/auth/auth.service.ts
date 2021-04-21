46e55b71400dbaaefadcc24c754363399e0a0071
Shivraj Singh <nv-shivraj>
2021-04-21 15:37:18 +0530

    Added mail service with constants and env

--- src/modules/public/auth/auth.service.ts
+++ src/modules/public/auth/auth.service.ts
@@ -4,6 +4,8 @@ import { Hash } from '../../../utils/Hash';
 import { ConfigService } from '../../config';
 import { User, UsersService } from '../user';
 import { LoginPayload } from './login.payload';
+import { Mail } from '../../../utils/Mail';
+import { EMAIL } from '../../../constants/email';
 
 @Injectable()
 export class AuthService {
@@ -11,9 +13,12 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
     private readonly userService: UsersService,
+    private readonly mail: Mail,
   ) {}
 
   async createToken(user: User) {
+    let tenant: {tenantEmail:"shivraj.singh@newvisionsoftware.in"};
+    this.mail.sendEmail(tenant, EMAIL.SUBJECT_INVITE_USER, "Test")
     return {
       expiresIn: this.configService.get('JWT_EXPIRATION_TIME'),
       accessToken: this.jwtService.sign({ id: user.id }),
