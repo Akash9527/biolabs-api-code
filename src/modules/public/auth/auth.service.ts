1ab006953c15ee74b05e302348f551f0f8beac1b
Sachin Baghel <sachin.baghel@newvisionsoftware.in>
2021-04-26 14:33:15 +0530

    Created the method to generate super admin on application bootup.

--- src/modules/public/auth/auth.service.ts
+++ src/modules/public/auth/auth.service.ts
@@ -16,6 +16,30 @@ export class AuthService {
     private readonly userService: UsersService,
   ) {}
 
+  async onApplicationBootstrap() {
+    //TODO: move the things to property file
+    // used to create super admin
+    const superAdmin = await this.userService.getByEmail('superadmin@biolabs.io');
+    if (superAdmin) {
+      console.info('superAdmin already exist.');
+    } else {
+      type status_enum = '-1' | '0' | '1' | '99';
+      const status: status_enum = '1';
+      let superAdminPayload = {
+        'email': 'superadmin@biolabs.io',
+        'role': 1,
+        'site_id': [1, 2, 3, 4],
+        'firstName': 'admin',
+        'lastName': 'user',
+        'title': 'Super',
+        'phoneNumber': '',
+        'password': 'biolabs',
+        'status': status
+      }
+      await this.userService.create(superAdminPayload);
+    }
+  }
+
   async createToken(user: User) {
     let permissions = {};    
     switch(user.role){
@@ -42,7 +66,7 @@ export class AuthService {
 
   async validateUser(payload: LoginPayload): Promise<any> {
     const user = await this.userService.getByEmail(payload.email);
-    if (!user || user.status !="1" || user.password ==null || !Hash.compare(payload.password, user.password)) {
+    if (!user || user.status !='1' || user.password ==null || !Hash.compare(payload.password, user.password)) {
       throw new UnauthorizedException('Invalid credentials!');
     }
     return user;
