8d4eb0b4d9c8558a42fca6768ae985f8f13e9471
shailesh <shailesh.shrivastava@newvisionsoftware.in>
2021-04-27 16:17:21 +0530

    Remove commented code and format the service.

--- src/modules/public/auth/auth.service.ts
+++ src/modules/public/auth/auth.service.ts
@@ -19,11 +19,9 @@ export class AuthService {
     private readonly configService: ConfigService,
     private readonly userService: UsersService,
     private readonly masterService: MasterService
-  ) {}
+  ) { }
 
   async onApplicationBootstrap() {
-    //TODO: move the things to property file
-    // used to create super admin
     await this.masterService.createRoles();
     await this.masterService.createSites();
     await this.createSuperAdmin();
@@ -41,16 +39,16 @@ export class AuthService {
   }
 
   async createToken(user: User) {
-    let permissions = {};    
-    switch(user.role){
+    let permissions = {};
+    switch (user.role) {
       case 1:
-        permissions=SUPER_ADMIN_ACCESSLEVELS;
+        permissions = SUPER_ADMIN_ACCESSLEVELS;
         break;
       case 2:
-        permissions=SITE_ADMIN_ACCESSLEVELS;
+        permissions = SITE_ADMIN_ACCESSLEVELS;
         break;
       case 3:
-        permissions=SPONSOR_ACCESSLEVELS;
+        permissions = SPONSOR_ACCESSLEVELS;
         break;
       default:
         break;
@@ -59,23 +57,23 @@ export class AuthService {
     return {
       expiresIn: this.configService.get('JWT_EXPIRATION_TIME'),
       accessToken: this.jwtService.sign({ id: user.id }),
-      permissions:permissions,
+      permissions: permissions,
       user,
     };
   }
 
   async validateUser(payload: LoginPayload): Promise<any> {
     const user = await this.userService.getByEmail(payload.email);
-    if (!user || user.status !='1' || user.password ==null || !Hash.compare(payload.password, user.password)) {
+    if (!user || user.status != '1' || user.password == null || !Hash.compare(payload.password, user.password)) {
       throw new UnauthorizedException('Invalid credentials!');
     }
     return user;
   }
 
-  async validateToken(token){
+  async validateToken(token) {
     return this.userService.validateToken(token);
   }
-  async forgotPassword(payload){
+  async forgotPassword(payload) {
     return this.userService.forgotPassword(payload);
   }
 }
