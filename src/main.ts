6e8d4e1681b70587da10a9c94948d3124299be01
shreyas <shreyas.bhargava@newvisionsoftware.in>
2021-05-10 19:08:41 +0530

    added console .log

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,20 @@ import { TrimStringsPipe } from 'modules/common/transformer/trim-strings.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+
+
+     
+
+
+  console.log('type = '+`${process.env.POSTGRESQL_DB_TYPE}`);
+  console.log('POSTGRESQL_DB_HOST = '+`${process.env.POSTGRESQL_DB_HOST}`);
+  console.log('POSTGRESQL_DB_PORT = '+`${process.env.POSTGRESQL_DB_PORT}`);
+  console.log('POSTGRESQL_DB_USERNAME = '+`${process.env.POSTGRESQL_DB_USERNAME}`);
+  console.log('POSTGRESQL_DB_PASSWORD = '+`${process.env.POSTGRESQL_DB_PASSWORD}`);
+  console.log('POSTGRESQL_DB_NAME = '+`${process.env.POSTGRESQL_DB_NAME}`);
+
+
+
   app.enableCors();
   app.useGlobalPipes(new TrimStringsPipe(), new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
