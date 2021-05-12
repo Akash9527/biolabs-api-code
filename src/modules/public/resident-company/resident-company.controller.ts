a2cbc4d77b774865ec6bcfcb4dcaee2b89ad10e2
Shivraj Singh <nv-shivraj>
2021-05-12 10:04:30 +0530

    Added formating and comment for the controllers

--- src/modules/public/resident-company/resident-company.controller.ts
+++ src/modules/public/resident-company/resident-company.controller.ts
@@ -25,6 +25,11 @@ export class ResidentCompanyController {
     private readonly residentCompanyService: ResidentCompanyService,
   ) { }
 
+  /**
+   * Description: This method is used to create a resident company.
+   * @description This method is used to create a resident company.
+   * @param payload it is a request body contains payload of type AddResidentCompanyPayload.
+   */
   @Post()
   @ApiResponse({ status: 200, description: 'Successful Response' })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
@@ -37,6 +42,11 @@ export class ResidentCompanyController {
     return user;
   }
 
+  /**
+   * Description: This method is used to list the resident companies by get method.
+   * @description This method is used to list the resident companies by get method.
+   * @param payload it is a request query expects the payload of type ?any.
+   */
   @ApiBearerAuth()
   @UseGuards(AuthGuard())
   @Get()
@@ -45,7 +55,12 @@ export class ResidentCompanyController {
   async getResidentCompanies(@Query() params: any): Promise<any> {
     return this.residentCompanyService.getResidentCompanies(params);
   }
-
+  
+  /**
+   * Description: This method is used to get a resident company information.
+   * @description This method is used to get a resident company information.
+   * @param id it is a request parameter expect a number value of resident company id.
+   */
   @ApiBearerAuth()
   @UseGuards(AuthGuard())
   @Get(':id')
@@ -55,6 +70,11 @@ export class ResidentCompanyController {
     return this.residentCompanyService.getResidentCompany(id);
   }
 
+  /**
+   * Description: This method is used to upadte a resident company status.
+   * @description This method is used to update a resident company status.
+   * @param payload it is a request body contains payload of type UpdateResidentCompanyStatusPayload.
+   */
   @ApiBearerAuth()
   @UseGuards(AuthGuard())
   @Put('/update-status')
