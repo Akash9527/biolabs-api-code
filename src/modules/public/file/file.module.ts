c9f1fc849378bf685d4081bdbeec87bf837d546e
Sumit Shrivastava <sumit.shrivastava@newvisionsoftware.in>
2021-05-14 13:34:39 +0530

    Moved the Azure Storage Connection configuration to env file and clean up

--- src/modules/public/file/file.module.ts
+++ src/modules/public/file/file.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '../../config';
+import { ConfigModule } from 'modules/config';
+import { UserModule } from '../user';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
-import { PassportModule } from '@nestjs/passport';
-import { UserModule } from '../user';
-import { Mail } from 'utils/Mail';
 
 @Module({
   imports: [
     UserModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }),],
+    ConfigModule
+  ],
   controllers: [FileController],
   providers: [FileService],
-  exports: [PassportModule.register({ defaultStrategy: 'jwt' }), FileService],
+  exports: [FileService],
 })
 export class FileModule {}
