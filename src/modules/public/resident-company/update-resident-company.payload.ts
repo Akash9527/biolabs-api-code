c90bf65efcb7e87e9b0ce63be8d08351d15ead9f
Shivraj Singh <nv-shivraj>
2021-05-23 12:45:05 +0530

    Removed unused imports

--- src/modules/public/resident-company/update-resident-company.payload.ts
+++ src/modules/public/resident-company/update-resident-company.payload.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsNumber, IsOptional, Matches, Max, MaxLength, Min, MinLength, ValidateIf } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, Matches, MaxLength, Min, MinLength, ValidateIf } from 'class-validator';
 
 export class UpdateResidentCompanyPayload {
   @ApiProperty({
