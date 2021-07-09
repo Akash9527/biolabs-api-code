c63517f08b6bc49d132ca52a48901f428e18f84b
Sachin Baghel <sachin.baghel@newvisionsoftware.in>
2021-07-09 13:25:10 +0530

    Updated the view for global search.

--- SQL/views/global_search_view.sql
+++ SQL/views/global_search_view.sql
@@ -1,32 +1,174 @@
 CREATE OR REPLACE VIEW GLOBAL_SEARCH_VIEW as
-SELECT 
-string_agg(distinct fun.name, ',') AS fundingSrcName,
-string_agg(distinct ind.name, ',') AS industryName,
-string_agg(distinct moda.name, ',') AS modalityName,
-string_agg(distinct s.name, ',') AS siteName,
-string_agg(distinct stage.name, ',') AS techStageName,
-string_agg(distinct bsrc.name, ',') AS bSourcesName,
-string_agg(distinct rca.name, ',') AS advisoryName,
-string_agg(distinct rca.title, ',') AS advisoryTitle,
-string_agg(distinct rca.organization, ',') AS advisoryOrg,
--- rca.name AS advisoryName, rca.title AS advisoryTitle, rca.organization AS advisoryOrg,
-rc.id, rc.email, rc.name, rc."companyName", rc.site, rc."biolabsSources", "otherBiolabsSources", technology, "rAndDPath", "startDate", "companySize", "foundedPlace", "companyStage", "otherCompanyStage", funding, "fundingSource", "otherFundingSource", "intellectualProperty", "otherIntellectualProperty", "isAffiliated", "affiliatedInstitution", "noOfFullEmp", "empExpect12Months", 
-rc."utilizeLab", rc."expect12MonthsUtilizeLab", rc."industry",
-rc."otherIndustries":: text,
-modality, "otherModality":: text, "preferredMoveIn", rc.status, "companyStatus", "companyVisibility", "companyOnboardingStatus", "elevatorPitch", "logoOnWall", "logoOnLicensedSpace", "bioLabsAssistanceNeeded", "technologyPapersPublished", "technologyPapersPublishedLinkCount", "technologyPapersPublishedLink", "patentsFiledGranted", "patentsFiledGrantedDetails", "foundersBusinessIndustryBefore", "academiaPartnerships", "academiaPartnershipDetails", "industryPartnerships", "industryPartnershipsDetails", newsletters, "shareYourProfile", "equipmentOnsite", website, "foundersBusinessIndustryName", rc."createdAt", rc."updatedAt", pitchdeck, "logoImgUrl", "committeeStatus", "selectionDate", "companyStatusChangeDate"
+SELECT
+  string_agg(distinct fun.name, ',') AS fundingSrcName,
+  string_agg(distinct ind.name, ',') AS industryName,
+  string_agg(distinct moda.name, ',') AS modalityName,
+  string_agg(distinct s.name, ',') AS siteName,
+  string_agg(distinct stage.name, ',') AS techStageName,
+  string_agg(distinct bsrc.name, ',') AS bSourcesName,
+ -------------- section for resident_company_advisory -------------
+  string_agg(distinct rca.name, ',') AS advisoryName,
+  string_agg(distinct rca.title, ',') AS advisoryTitle,
+  string_agg(distinct rca.organization, ',') AS advisoryOrg,
+ -------------- section for resident_company_management -------------
+  string_agg(distinct rcm.name, ',') AS mgmtname,
+  string_agg(distinct rcm.title, ',') AS mgmttitle,
+  string_agg(distinct rcm.email, ',') AS mgmtemail,
+  string_agg(distinct rcm.phone, ',') AS mgmtphone,
+  string_agg(distinct rcm."linkedinLink", ',') AS mgmtlinkedin,
+  string_agg(distinct rcm.publications, ',') AS mgmtpublications,
+  string_agg(distinct rcm."academicAffiliation", ',') AS mgmtffiliation,
+--   string_agg(distinct rcm."joiningAsMember"::text, ',') AS mgmjoining,
+ ------------- section for resident_company_technical -------------
+  string_agg(distinct rct.name, ',') AS techname,
+  string_agg(distinct rct.title, ',') AS techtitle,
+  string_agg(distinct rct.email, ',') AS techemail,
+  string_agg(distinct rct.phone, ',') AS techphone,
+  string_agg(distinct rct."linkedinLink", ',') AS techlinkedin,
+  string_agg(distinct rct.publications, ',') AS techpublications,
 
-FROM public.resident_companies rc
-LEFT JOIN "fundings" fun ON fun.id = Any("fundingSource")
-LEFT JOIN "categories" ind ON ind.id = Any("industry")
-LEFT JOIN "modalities" moda ON moda.id = Any("modality")
-LEFT JOIN "sites" s ON s.id = Any("site")
-LEFT JOIN "technology_stages" stage ON stage.id = rc."companyStage"
-LEFT JOIN "biolabs_sources" bsrc ON bsrc.id = rc."biolabsSources"
-LEFT JOIN "resident_company_advisory" rca ON rca."companyId" = rc."id"
--- WHERE "comnpanyId" = 4
-GROUP BY 
-rc.id, rc.email, rc.name, rc."companyName", rc.site, rc."biolabsSources",  technology, "rAndDPath", "startDate", "companySize", "foundedPlace", "companyStage", "otherCompanyStage", funding, "fundingSource", "otherFundingSource", "intellectualProperty", "otherIntellectualProperty", "isAffiliated", "affiliatedInstitution", "noOfFullEmp", "empExpect12Months", 
-rc."utilizeLab", rc."expect12MonthsUtilizeLab", rc."industry",
-rc."otherIndustries":: text,
-modality, "otherModality":: text, "preferredMoveIn", rc.status, "companyStatus", "companyVisibility", "companyOnboardingStatus", "elevatorPitch", "logoOnWall", "logoOnLicensedSpace", "bioLabsAssistanceNeeded", "technologyPapersPublished", "technologyPapersPublishedLinkCount", "technologyPapersPublishedLink", "patentsFiledGranted", "patentsFiledGrantedDetails", "foundersBusinessIndustryBefore", "academiaPartnerships", "academiaPartnershipDetails", "industryPartnerships", "industryPartnershipsDetails", newsletters, "shareYourProfile", "equipmentOnsite", website, "foundersBusinessIndustryName", rc."createdAt", rc."updatedAt", pitchdeck, "logoImgUrl", "committeeStatus", "selectionDate", "companyStatusChangeDate"
--- rca.name, rca.title, rca.organization
+  -- rca.name AS advisoryName, rca.title AS advisoryTitle, rca.organization AS advisoryOrg,
+  rc.id,
+  rc.email,
+  rc.name,
+  rc."companyName",
+  rc.site,
+  rc."biolabsSources",
+  "otherBiolabsSources",
+  technology,
+  "rAndDPath",
+  "startDate",
+  "companySize",
+  "foundedPlace",
+  "companyStage",
+  "otherCompanyStage",
+  funding,
+  "fundingSource",
+  "otherFundingSource",
+  "intellectualProperty",
+  "otherIntellectualProperty",
+  "isAffiliated",
+  "affiliatedInstitution",
+  "noOfFullEmp",
+  "empExpect12Months",
+  rc."utilizeLab",
+  rc."expect12MonthsUtilizeLab",
+  rc."industry",
+  rc."otherIndustries" :: text,
+  modality,
+  "otherModality" :: text,
+  "preferredMoveIn",
+  rc.status,
+  "companyStatus",
+  (
+    CASE WHEN "companyStatus" = '0' THEN 'Applied' WHEN "companyStatus" = '1' THEN 'Current Member' WHEN "companyStatus" = '2' THEN 'On Hold' WHEN "companyStatus" = '3' THEN 'Discontinued' WHEN "companyStatus" = '4' THEN 'Graduated' ELSE "companyStatus" END
+  ) as companystatustext,
+  "companyVisibility",
+  (
+    CASE WHEN "companyVisibility" = true THEN 'Published' ELSE 'Stealth' END
+  ) as companyvisibilitytext,
+  "companyOnboardingStatus",
+  (
+    CASE WHEN "companyOnboardingStatus" = true THEN 'Onboarded' ELSE 'Needs Onboarding' END
+  ) as companyonboardingtext,
+  "elevatorPitch",
+  "logoOnWall",
+  "logoOnLicensedSpace",
+  "bioLabsAssistanceNeeded",
+  "technologyPapersPublished",
+  "technologyPapersPublishedLinkCount",
+  "technologyPapersPublishedLink",
+  "patentsFiledGranted",
+  "patentsFiledGrantedDetails",
+  "foundersBusinessIndustryBefore",
+  "academiaPartnerships",
+  "academiaPartnershipDetails",
+  "industryPartnerships",
+  "industryPartnershipsDetails",
+  newsletters,
+  "shareYourProfile",
+  "equipmentOnsite",
+  website,
+  "foundersBusinessIndustryName",
+  rc."createdAt",
+  rc."updatedAt",
+  pitchdeck,
+  "logoImgUrl",
+  "committeeStatus",
+  (
+    CASE WHEN "committeeStatus" = '0' THEN 'Unscheduled' WHEN "committeeStatus" = '1' THEN 'Scheduled' WHEN "committeeStatus" = '2' THEN 'Accepted' WHEN "committeeStatus" = '3' THEN 'Rejected' ELSE "committeeStatus" END
+  ) as committeestatustext,
+  "selectionDate",
+  "companyStatusChangeDate"
+FROM
+  public.resident_companies rc
+  LEFT JOIN "fundings" fun ON fun.id = Any("fundingSource")
+  LEFT JOIN "categories" ind ON ind.id = Any("industry")
+  LEFT JOIN "modalities" moda ON moda.id = Any("modality")
+  LEFT JOIN "sites" s ON s.id = Any("site")
+  LEFT JOIN "technology_stages" stage ON stage.id = rc."companyStage"
+  LEFT JOIN "biolabs_sources" bsrc ON bsrc.id = rc."biolabsSources"
+  LEFT JOIN "resident_company_advisory" rca ON rca."companyId" = rc."id" 
+  LEFT JOIN "resident_company_management" rcm ON rcm."companyId" = rc."id" 
+  LEFT JOIN "resident_company_technical" rct ON rct."companyId" = rc."id" 
+  -- WHERE "comnpanyId" = 4
+GROUP BY
+  rc.id,
+  rc.email,
+  rc.name,
+  rc."companyName",
+  rc.site,
+  rc."biolabsSources",
+  technology,
+  "rAndDPath",
+  "startDate",
+  "companySize",
+  "foundedPlace",
+  "companyStage",
+  "otherCompanyStage",
+  funding,
+  "fundingSource",
+  "otherFundingSource",
+  "intellectualProperty",
+  "otherIntellectualProperty",
+  "isAffiliated",
+  "affiliatedInstitution",
+  "noOfFullEmp",
+  "empExpect12Months",
+  rc."utilizeLab",
+  rc."expect12MonthsUtilizeLab",
+  rc."industry",
+  rc."otherIndustries" :: text,
+  modality,
+  "otherModality" :: text,
+  "preferredMoveIn",
+  rc.status,
+  "companyStatus",
+  "companyVisibility",
+  "companyOnboardingStatus",
+  "elevatorPitch",
+  "logoOnWall",
+  "logoOnLicensedSpace",
+  "bioLabsAssistanceNeeded",
+  "technologyPapersPublished",
+  "technologyPapersPublishedLinkCount",
+  "technologyPapersPublishedLink",
+  "patentsFiledGranted",
+  "patentsFiledGrantedDetails",
+  "foundersBusinessIndustryBefore",
+  "academiaPartnerships",
+  "academiaPartnershipDetails",
+  "industryPartnerships",
+  "industryPartnershipsDetails",
+  newsletters,
+  "shareYourProfile",
+  "equipmentOnsite",
+  website,
+  "foundersBusinessIndustryName",
+  rc."createdAt",
+  rc."updatedAt",
+  pitchdeck,
+  "logoImgUrl",
+  "committeeStatus",
+  "selectionDate",
+  "companyStatusChangeDate" -- rca.name, rca.title, rca.organization
