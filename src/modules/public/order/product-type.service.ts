00a05324a73bb75d135116435f6774d67848fe06
RutujaBagade <rutuja.bagade@newvisionsoftware.in>
2021-07-09 18:56:34 +0530

    Added logger in product type

--- src/modules/public/order/product-type.service.ts
+++ src/modules/public/order/product-type.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AddProductTypeDto } from './dto/AddProductType.dto';
 import { ProductType } from './model/product-type.entity';
+const {error, warn, info,debug}=require("../../../utils/logger")
+const {ResourceNotFoundException,InternalException,BiolabsException} = require('../../common/exception/biolabs-error');
 
 @Injectable()
 export class ProductTypeService {
@@ -19,6 +21,7 @@ export class ProductTypeService {
      * @return saved order product object
      */
     async addProductType(addProductTypeDto: AddProductTypeDto) {
+        info("Adding product Type Name:"+addProductTypeDto.productTypeName,__filename,"addProductType()")
         return await this.productTypeRepository.save(this.productTypeRepository.create(addProductTypeDto));
     }
 
@@ -28,6 +31,7 @@ export class ProductTypeService {
      * @returns All product type
      */
     async getProductType() {
+        info("Getting product Type",__filename,"addProductType()")
         return await await this.productTypeRepository.find();
     }
 }
